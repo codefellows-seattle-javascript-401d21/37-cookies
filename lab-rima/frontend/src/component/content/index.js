84236425ffe8a1d040e10eccaef9e92296ed42d0
Rima <rmhrok@gmail.com>
2018-03-07 03:45:53 +0000

    change text in content page

--- lab-rima/frontend/src/component/content/index.js
+++ lab-rima/frontend/src/component/content/index.js
@@ -4,8 +4,7 @@ export default class Content extends React.Component {
   render(){
     return (
       <div>
-        <h1>You are authorized!</h1>
-        <p>TOKEN: to remove later: {this.props.token}</p>
+        <h1>You are logged in!</h1>
       </div>
     )
   }
