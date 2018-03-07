25425ba3c119da16038415a86b81f641a6b1db33
Rima <rmhrok@gmail.com>
2018-03-07 05:43:12 +0000

    scaffold util test

---
+++ lab-rima/frontend/src/__test__/lib/utils.test.js
@@ -0,0 +1,13 @@
+import utils from '../../lib/utils';
+
+describe('utils module', () => {
+
+  describe('map function', () => {
+  });
+
+  describe('filter function', () => {
+  });
+
+  describe('reduce function', () => {
+  });
+});
